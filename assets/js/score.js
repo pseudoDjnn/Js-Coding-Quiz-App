3005e7d3da8572504ee4f864788cbc04ca68508e
p5eudoDjnn <alexander.havers@gmail.com>
2022-08-06 18:50:25 -0400

    fix 2

--- assets/js/score.js
+++ assets/js/score.js
@@ -41,7 +41,7 @@ saveHighScore = (event) => {
 
   // Save string with JSON
   localStorage.setItem("high-score", JSON.stringify(highScores));
-  window.location.assign("/");
+  window.location.assign("/leaderboards.html");
 
   console.log(highScores);
 };
